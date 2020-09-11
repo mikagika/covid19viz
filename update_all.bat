76729b63446a5da4c002123e08a9031980314a33
Scott Chapman <geek@mikagika.com>
2020-09-11 05:02:19 -0400

    Automatic data updates

--- update_all.bat
+++ update_all.bat
@@ -1,2 +1,2 @@
-sed -i '/2020-09-09/d' html/data/covid19_daily_reports_all.csv 
-grep "2020-09-09" html/data/covid19_daily_reports.csv >>html/data/covid19_daily_reports_all.csv 
+sed -i '/2020-09-10/d' html/data/covid19_daily_reports_all.csv 
+grep "2020-09-10" html/data/covid19_daily_reports.csv >>html/data/covid19_daily_reports_all.csv 
