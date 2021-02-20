8d543130a3fe5c468fc461c02177809daae8ad81
mikagika <geek@mikagika.com>
2021-02-20 10:09:36 -0500

    added 2-13

--- update_all.bat
+++ update_all.bat
@@ -1,2 +1,2 @@
-sed -i '/2021-02-12/d' html/data/covid19_daily_reports_all.csv 
-grep "2021-02-12" html/data/covid19_daily_reports.csv >>html/data/covid19_daily_reports_all.csv 
+sed -i '/2021-02-13/d' html/data/covid19_daily_reports_all.csv 
+grep "2021-02-13" html/data/covid19_daily_reports.csv >>html/data/covid19_daily_reports_all.csv 
