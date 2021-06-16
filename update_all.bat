d5b36abe7acff656ab251348d4447564b1a411ea
mikagika <geek@mikagika.com>
2021-06-16 07:32:16 -0400

    Automatic data updates

--- update_all.bat
+++ update_all.bat
@@ -1,2 +1,2 @@
-sed -i '/2021-06-14/d' html/data/covid19_daily_reports_all.csv 
-grep "2021-06-14" html/data/covid19_daily_reports.csv >>html/data/covid19_daily_reports_all.csv 
+sed -i '/2021-06-15/d' html/data/covid19_daily_reports_all.csv 
+grep "2021-06-15" html/data/covid19_daily_reports.csv >>html/data/covid19_daily_reports_all.csv 
